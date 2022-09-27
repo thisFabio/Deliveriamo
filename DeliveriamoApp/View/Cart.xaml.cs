18aa512a3c63b172189eb6097ad6dc0f1f54074f
thisFabio <74615845+thisFabio@users.noreply.github.com>
2022-09-27 07:33:53 +0200

    Add Cart WIP

--- DeliveriamoApp/View/Cart.xaml.cs
+++ DeliveriamoApp/View/Cart.xaml.cs
@@ -6,4 +6,9 @@ public partial class Cart : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+
+	}
 }
--- DeliveriamoApp/ViewModel/CartViewModel.cs
+++ DeliveriamoApp/ViewModel/CartViewModel.cs
@@ -3,8 +3,10 @@ using DeliveriamoClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DeliveriamoApp.ViewModel
 {
@@ -12,6 +14,19 @@ namespace DeliveriamoApp.ViewModel
     {
 
         private List<ProductDto> productList = ((App)App.Current).CartItems;
+        private ProductDto selectedProduct;
+
+        public ProductDto SelectedProduct 
+        { 
+            get
+            {
+                return selectedProduct;
+            } set 
+            { 
+                this.SetProperty(ref selectedProduct, value);
+
+            }
+        }
 
         public List<ProductDto> ProductList
         {
@@ -22,10 +37,14 @@ namespace DeliveriamoApp.ViewModel
             set
             {
                 this.SetProperty(ref productList, value);
-
             }
 
         }
+
+        public ICommand RemoveFromCart => new Command(async () => // definisco il tipo di comando e il suo datatype in INPUT
+        {
+            ProductList.Remove(SelectedProduct);
+        });
 
 
     }
